9bc4145d54b7cc49369d23dd0d0fb1d81028a282
Vladimir <vladimir.koval.kz@gmail.com>
2024-07-06 23:17:12 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,102 @@
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true
+  });
+
+  // Получить модальный
+const modal = document.getElementById("myModal");
+            
+// Получить кнопку, которая открывает модальный
+const btn = document.getElementById("myBtn")
+
+// Получить элемент <span>, который закрывает модальный
+const span = document.getElementsByClassName("close")[0];
+
+// Когда пользователь нажимает на кнопку, откройте модальный
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// Когда пользователь нажимает на <span> (x), закройте модальное окно
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+  // Получить модальный
+const modal1 = document.getElementById("myModal1");
+            
+// Получить кнопку, которая открывает модальный
+const btn1 = document.getElementById("myBtn1")
+
+// Получить элемент <span>, который закрывает модальный
+const span1 = document.getElementsByClassName("close1")[0];
+
+// Когда пользователь нажимает на кнопку, откройте модальный
+btn1.onclick = function() {
+  modal1.style.display = "block";
+}
+
+// Когда пользователь нажимает на <span> (x), закройте модальное окно
+span1.onclick = function() {
+  modal1.style.display = "none";
+}
+
+  // Получить модальный
+const modal2 = document.getElementById("myModal2");
+            
+// Получить кнопку, которая открывает модальный
+const btn2 = document.getElementById("myBtn2")
+
+// Получить элемент <span>, который закрывает модальный
+const span2 = document.getElementsByClassName("close2")[0];
+
+// Когда пользователь нажимает на кнопку, откройте модальный
+btn2.onclick = function() {
+  modal2.style.display = "block";
+}
+
+// Когда пользователь нажимает на <span> (x), закройте модальное окно
+span2.onclick = function() {
+  modal2.style.display = "none";
+}
+
+  // Получить модальный
+const modal3 = document.getElementById("myModal3");
+            
+// Получить кнопку, которая открывает модальный
+const btn3 = document.getElementById("myBtn3")
+
+// Получить элемент <span>, который закрывает модальный
+const span3 = document.getElementsByClassName("close3")[0];
+
+// Когда пользователь нажимает на кнопку, откройте модальный
+btn3.onclick = function() {
+  modal3.style.display = "block";
+}
+
+// Когда пользователь нажимает на <span> (x), закройте модальное окно
+span3.onclick = function() {
+  modal3.style.display = "none";
+}
+
+  // Получить модальный
+const modal4 = document.getElementById("myModal4");
+            
+// Получить кнопку, которая открывает модальный
+const btn4 = document.getElementById("myBtn4")
+
+// Получить элемент <span>, который закрывает модальный
+const span4 = document.getElementsByClassName("close4")[0];
+
+// Когда пользователь нажимает на кнопку, откройте модальный
+btn4.onclick = function() {
+  modal4.style.display = "block";
+}
+
+// Когда пользователь нажимает на <span> (x), закройте модальное окно
+span4.onclick = function() {
+  modal4.style.display = "none";
+}
+
+  
