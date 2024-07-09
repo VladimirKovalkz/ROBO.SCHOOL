8f9ac6918975a796c43292857e3f1275ac1bbf48
Vladimir <vladimir.koval.kz@gmail.com>
2024-07-09 19:34:59 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,16 +2,16 @@ const swiper = new Swiper('.swiper', {
   slidesPerView: 3,
   spaceBetween: 30,
   loop: true,
-  
+
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-  });
+});
 
-  // Получить модальный
+// Получить модальный
 const modal = document.getElementById("myModal");
-            
+
 // Получить кнопку, которая открывает модальный
 const btn = document.getElementById("myBtn")
 
@@ -19,18 +19,18 @@ const btn = document.getElementById("myBtn")
 const span = document.getElementsByClassName("close")[0];
 
 // Когда пользователь нажимает на кнопку, откройте модальный
-btn.onclick = function() {
+btn.onclick = function () {
   modal.style.display = "block";
 }
 
 // Когда пользователь нажимает на <span> (x), закройте модальное окно
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
 }
 
-  // Получить модальный
+// Получить модальный
 const modal1 = document.getElementById("myModal1");
-            
+
 // Получить кнопку, которая открывает модальный
 const btn1 = document.getElementById("myBtn1")
 
@@ -38,18 +38,18 @@ const btn1 = document.getElementById("myBtn1")
 const span1 = document.getElementsByClassName("close1")[0];
 
 // Когда пользователь нажимает на кнопку, откройте модальный
-btn1.onclick = function() {
+btn1.onclick = function () {
   modal1.style.display = "block";
 }
 
 // Когда пользователь нажимает на <span> (x), закройте модальное окно
-span1.onclick = function() {
+span1.onclick = function () {
   modal1.style.display = "none";
 }
 
-  // Получить модальный
+// Получить модальный
 const modal2 = document.getElementById("myModal2");
-            
+
 // Получить кнопку, которая открывает модальный
 const btn2 = document.getElementById("myBtn2")
 
@@ -57,18 +57,18 @@ const btn2 = document.getElementById("myBtn2")
 const span2 = document.getElementsByClassName("close2")[0];
 
 // Когда пользователь нажимает на кнопку, откройте модальный
-btn2.onclick = function() {
+btn2.onclick = function () {
   modal2.style.display = "block";
 }
 
 // Когда пользователь нажимает на <span> (x), закройте модальное окно
-span2.onclick = function() {
+span2.onclick = function () {
   modal2.style.display = "none";
 }
 
-  // Получить модальный
+// Получить модальный
 const modal3 = document.getElementById("myModal3");
-            
+
 // Получить кнопку, которая открывает модальный
 const btn3 = document.getElementById("myBtn3")
 
@@ -76,18 +76,18 @@ const btn3 = document.getElementById("myBtn3")
 const span3 = document.getElementsByClassName("close3")[0];
 
 // Когда пользователь нажимает на кнопку, откройте модальный
-btn3.onclick = function() {
+btn3.onclick = function () {
   modal3.style.display = "block";
 }
 
 // Когда пользователь нажимает на <span> (x), закройте модальное окно
-span3.onclick = function() {
+span3.onclick = function () {
   modal3.style.display = "none";
 }
 
-  // Получить модальный
+// Получить модальный
 const modal4 = document.getElementById("myModal4");
-            
+
 // Получить кнопку, которая открывает модальный
 const btn4 = document.getElementById("myBtn4")
 
@@ -95,13 +95,12 @@ const btn4 = document.getElementById("myBtn4")
 const span4 = document.getElementsByClassName("close4")[0];
 
 // Когда пользователь нажимает на кнопку, откройте модальный
-btn4.onclick = function() {
+btn4.onclick = function () {
   modal4.style.display = "block";
 }
 
 // Когда пользователь нажимает на <span> (x), закройте модальное окно
-span4.onclick = function() {
+span4.onclick = function () {
   modal4.style.display = "none";
 }
 
-  
